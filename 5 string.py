9024e69e4dc6159f58c8e159b53ccd09ecdd0a76
21bca65 <96609180+21bca65@users.noreply.github.com>
2022-03-24 11:03:05 +0530

    Delete 5 string.py

--- 5 string.py
+++
@@ -1,9 +0,0 @@
-# was to enter five string in a list and check and print string whose length has even num of character
-l=[]
-for i in range(5):
-        s=input("enter string:")
-        l.append(s)
-        print(l)
-        
-        
-
